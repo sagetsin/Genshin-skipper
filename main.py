275dbfe94f0daa7149fefa016038159686473847
Sydney Tsin <sage.tsin@gmail.com>
2024-12-24 01:05:22 -0500

    Identify if dialog option and click

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pyscreeze
 import time
 
 def print_pos():
@@ -10,15 +11,29 @@ def move_mouse(x, y):
     pyautogui.moveTo(x,y)
 
 def click_auto():
-    pyautogui.click(x=33, y=32, buttom="left")
+    pyautogui.click(x=33, y=32, button="left")
 
 def forward_dialog():
-    for i in range(10):
-        pyautogui.click(x=942, y=664, button="left", clicks=1, interval=0.25)
+    pyautogui.click(x=942, y=664, button="left", clicks=1, interval=0.25)
+    print("click")
+
+def choose_dialog():
+    #im = pyautogui.screenshot("partial-screen.png", region=(905, 569, 1794, 943))
+    try:
+        x,y = pyautogui.locateCenterOnScreen("F.png")
+        pyautogui.press('f')
+    except:
+        print("No dialog found")
+
+
+def pause_game():
+    pyautogui.press('esc')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    time.sleep(5)
     #print_pos()
-    #move_mouse(33,32)
     click_auto()
-    forward_dialog()
+    for i in range(10):
+        forward_dialog()
+        choose_dialog()
