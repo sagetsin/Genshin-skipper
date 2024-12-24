5f6fc3829cea249606fa06f03cf124d19aae68d1
Sydney Tsin <sage.tsin@gmail.com>
2024-12-24 00:28:58 -0500

    Automate clicking and auto

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import pyautogui
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_pos():
+    for i in range(20):
+        print(pyautogui.position())
+        time.sleep(0.5)
 
+def move_mouse(x, y):
+    pyautogui.moveTo(x,y)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def click_auto():
+    pyautogui.click(x=33, y=32, buttom="left")
 
+def forward_dialog():
+    for i in range(10):
+        pyautogui.click(x=942, y=664, button="left", clicks=1, interval=0.25)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #print_pos()
+    #move_mouse(33,32)
+    click_auto()
+    forward_dialog()
